a56bb82116c9df192d3ccf0ef1eec803a0bc44a7
Nehuen Franco <teonehuen409@gmail.com>
2024-07-15 16:55:30 -0300

    initializing

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Incoming from './Components/Incoming/Incoming';
+
+function App() {
+  return (
+    <div className="App">
+      <Incoming />
+    </div>
+  );
+}
+
+export default App;
